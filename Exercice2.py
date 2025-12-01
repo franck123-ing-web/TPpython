7162860e8a17ffe885b800d9a2d3f84165f16ffd
franck123-ing-web <gamaspeed380@gmail.com>
2025-12-01 14:43:27 +0100

    Update Exercice2.py

--- Exercice2.py
+++ Exercice2.py
@@ -2,7 +2,7 @@ a = int(input("Entre le nombre 1: "))
 b = int(input("Entre le nombre 2: "))
 c = int(input("Entre le nombre 3: "))
 
-nombres = [a, b, c]   # On met les trois nombres dans une liste, on trie,
+nombres = [a, b, c]   # je met les trois nombres dans une liste puis je fais le trie
 nombres.sort() # la médiane sera l'élément du milieu (index 1)
 
 mediane = nombres[1]  # l'élément du milieu après tri
